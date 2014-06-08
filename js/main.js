7c61a2f402c44c4bcc2903b9745d4c56f5be1099
Dr.Yami <cuongnt.hn@gmail.com>
2014-06-08 21:19:12 +0300

    nearly done

--- js/main.js
+++ js/main.js
@@ -1,7 +1,14 @@
 $(function() {
-    $(".header").css("height", $(window).height() - 128);
+    $(".header").css("height", $(window).height() - 52);
 });
 
 $(window).resize(function() {
-    $(".header").css("height", $(window).height() - 128);
+    $(".header").css("height", $(window).height() - 52);
+});
+
+$('a').click(function(){
+    $('html, body').animate({
+        scrollTop: $( $.attr(this, 'href') ).offset().top - 64
+    }, 500);
+    return false;
 });
