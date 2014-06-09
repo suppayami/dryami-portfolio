5254861df30c3020bfe1229b21999e5193555b36
Dr.Yami <cuongnt.hn@gmail.com>
2014-06-10 00:30:48 +0300

    fixed project hover

--- js/main.js
+++ js/main.js
@@ -1,29 +1,4 @@
-$(function() {
-    $(".header").css("height", $(window).height() - 52);
-});
-
-$(window).resize(function() {
-    $(".header").css("height", $(window).height() - 52);
-});
-
-$('a').click(function(){
-    $('html, body').animate({
-        scrollTop: $( $.attr(this, 'href') ).offset().top - 64
-    }, 500);
-    return false;
-});
-
-$(".project-wrapper").hover(function() {
-    $(this).find(".project-button").removeClass("fadeOutUp");
-    $(this).find(".overlay").removeClass("fadeOut");
-    $(this).find(".project-button").addClass("animated bounceInUp");
-    $(this).find(".project-button").css("display", "block");
-    $(this).find(".overlay").addClass("animated fadeIn");
-    $(this).find(".overlay").css("display", "block");
-}, function() {
-    $(this).find(".project-button").addClass("fadeOutUp");
-    $(this).find(".overlay").addClass("fadeOut");
-});
+$(".header").css("height", $(window).height() - 52);
 
 var app = angular.module('app', ['ngRoute'], function($interpolateProvider) {
     $interpolateProvider.startSymbol('<%');
@@ -32,12 +7,27 @@ var app = angular.module('app', ['ngRoute'], function($interpolateProvider) {
 
 app.controller("PortfolioController", function($scope) {
     $(".header").css("height", $(window).height() - 52);
+    $(window).resize(function() {
+        $(".header").css("height", $(window).height() - 52);
+    });
 
     $scope.gotoSection = function(section) {
         $('html, body').animate({
             scrollTop: $(section).offset().top - 64
         }, 500);
     };
+
+    $(".project-wrapper").hover(function() {
+        $(this).find(".project-button").removeClass("fadeOutUp");
+        $(this).find(".overlay").removeClass("fadeOut");
+        $(this).find(".project-button").addClass("animated bounceInUp");
+        $(this).find(".project-button").css("display", "block");
+        $(this).find(".overlay").addClass("animated fadeIn");
+        $(this).find(".overlay").css("display", "block");
+    }, function() {
+        $(this).find(".project-button").addClass("fadeOutUp");
+        $(this).find(".overlay").addClass("fadeOut");
+    });
 });
 
 app.config(function($routeProvider, $locationProvider) {
